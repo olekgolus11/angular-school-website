a2a0d3f1b259e8b27faf60427ce67406998f690a
Olek Golus <olekgolus02@gmail.com>
2022-05-29 11:48:01 +0200

    - backend working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {UsersDataService} from './services/users-data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projektWWW';
+  users: any;
+
+  constructor(private userData: UsersDataService) {
+    this.userData.users().subscribe((data) => {
+      this.users = data;
+    })
+  }
+  getUserFormData(data:any)
+  {
+    console.warn(data);
+    this.userData.saveUser(data).subscribe((result)=>{
+      console.warn(result);
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HttpClientModule } from '@angular/common/http';
-import {FormsModule} from '@angular/forms'
-
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
