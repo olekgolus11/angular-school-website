93d4069d866faf965cd71f5fb86656d3e0f9bb95
Olek Golus <olekgolus02@gmail.com>
2022-05-29 10:53:22 +0200

    - starting backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import {FormsModule} from '@angular/forms'
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
